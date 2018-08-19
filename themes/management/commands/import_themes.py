18615e907d6f246fd51b3c1ff8a1f280033c69a6
Aarnav Bos <aarnavbos@gmail.com>
2018-08-19 19:01:10 +0530

    Added a theme directory and a way to import themes to the DB

---
+++ themes/management/commands/import_themes.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand, CommandError
+from themes.themes import THEME_LIST
+from themes.models import Theme
+
+class Command(BaseCommand):
+    help = "Imports all themes if they don't exist, used for adding new themes."
+
+    def handle(self, *args, **options):
+        for theme in THEME_LIST:
+            if Theme.objects.filter(pk=theme['uuid']).count() != 1:
+                # if not one, import theme
+                Theme.objects.create(id=theme['uuid'], name=theme['name'])
+                self.stdout.write(self.style.SUCCESS('Successfully imported theme \
+                {}'.format(theme['name'])))
+            else:
+                self.stdout.write(self.style.SUCCESS('ignoring theme {} \
+                 as it already exists'.format(theme['name'])))
---
+++ themes/themes.py
@@ -0,0 +1,15 @@
+# Update this everytime a new theme is added.
+# The schema is the following:
+'''
+    {
+        'name': <theme_name>
+        'uuid': <hardcoded_id>
+    }
+'''
+
+THEME_LIST = [
+    {
+        'name': 'basic',
+        'uuid': 'de9d76bb-14fa-45c1-9a99-d01b25414ce8'
+    },
+]
