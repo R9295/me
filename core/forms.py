d48a0a134bcf20b97f8f593a988de72fc520426e
Aarnav Bos <aarnavbos@gmail.com>
2018-08-20 16:26:12 +0530

    added styling to profile.html, with django supplying css classes through the modelform

--- core/forms.py
+++ core/forms.py
@@ -3,8 +3,40 @@ from django.core.exceptions import ValidationError
 
 from .models import Profile
 
+FIELD_CLASSES = {
+    'prefix': 'uk-input',
+    'first_name': 'uk-input',
+    'last_name': 'uk-input',
+    'short_description': 'uk-input',
+    'description': 'uk-textarea',
+    'github': 'uk-input',
+    'facebook': 'uk-input',
+    'twitter': 'uk-input',
+    'medium': 'uk-input',
+    'linkedin': 'uk-input',
+    'theme': 'uk-select',
+}
 
+FIELD_ICONS = {
+    'prefix': 'home',
+    'first_name': 'user',
+    'last_name': 'user',
+    'short_description': 'info',
+    'github': 'github',
+    'facebook': 'facebook',
+    'twitter': 'twitter',
+    'medium': 'medium',
+    'linkedin': 'linkedin',
+}
 class ProfileForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(ProfileForm, self).__init__(*args, **kwargs)
+        # set classes
+        for k, v in self.fields.items():
+            v.widget.attrs['class'] = FIELD_CLASSES.get(k)
+            v.widget.icon = FIELD_ICONS.get(k)
+
 
     def clean(self):
         cleaned_data = super(ProfileForm, self).clean()
@@ -20,4 +52,7 @@ class ProfileForm(forms.ModelForm):
                   'medium', 'twitter', 'linkedin', 'theme',)
         widgets = {
             'user': forms.HiddenInput()
+        }
+        help_texts = {
+            'prefix': 'Your unique url'
         }
