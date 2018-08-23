5e4b081e3a2a8fb0c4f46bf0bf44963d152a28a2
Aditya Giri <dtrg21@gmail.com>
2018-08-23 16:21:57 +0530

    Add card theme

--- themes/themes.py
+++ themes/themes.py
@@ -12,4 +12,8 @@ THEME_LIST = [
         'name': 'basic',
         'uuid': 'de9d76bb-14fa-45c1-9a99-d01b25414ce8'
     },
+    {
+        'name': 'card',
+        'uuid': 'bc5ac915-97a8-40d7-8598-62d0e2554de0'
+    }
 ]
